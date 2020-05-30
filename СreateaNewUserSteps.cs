2c8455b01682600bef2252d983d6ff924312ffab
katherinetishevich <61135659+katherinetishevich@users.noreply.github.com>
2020-05-30 20:06:14 +0300

    HomeWork1
    HomeWork

---
+++ СreateaNewUserSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary1..........ClassLibrary1.ClassLibrary1
+{
+    [Binding]
+    public class СreateaNewUserSteps
+    {
+        [Given(@"I have  prepored  post request")]
+        public void GivenIHavePreporedPostRequest()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have a problem with data validation")]
+        public void GivenIHaveAProblemWithDataValidation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press send button")]
+        public void WhenIPressSendButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be add new user on the screen")]
+        public void ThenTheResultShouldBeAddNewUserOnTheScreen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
